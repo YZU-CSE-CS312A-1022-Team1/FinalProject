afdcdae3c1a61da50f241cd68136f2ea5b0ab1f4
Amo <t9525838@gmail.com>
2014-07-21 00:23:31 +0800

    add permission check

--- textVersion/AllUserInfo.sh
+++ textVersion/AllUserInfo.sh
@@ -20,67 +20,23 @@ if [ -f userInformation.txt ];then
     rm userInformation.txt
 fi
 
-# Program : rank all user's commands
-
-TOTAL=0    #Count how many commands
-TEMP="$(mktemp rankSpaceResult.XXXXXXX)"
-TEMPSEC="$(mktemp rankSpaceResult.XXXXXXX)"
 
 if [ `whoami` == "root" ];then
     users=$(last | awk '{if (( NF >= 10 ) && ($1 != "reboot")) print $1}'| sort | uniq)
-else
+  else
     users=`whoami`
+    ${ECHO} "You do not have root permission, program will only list login ranking and login time distribution."
 fi
-
-for username in $users
-do
-    USER=$username
-    HOME=`eval echo ~${USER}`
-
-    if [ ${HOME}/.bash_history -nt ${HOME}/.history ] && \
-       [ ${HOME}/.bash_history -nt ${HOME}/.histfile ] && \
-       [ ${HOME}/.bash_history -nt ${HOME}/.sh_history ]; then
-       LATESTHISTORY=`echo "${HOME}/.bash_history"`
-    elif [ ${HOME}/.history -nt ${HOME}/.bash_history ] && \
-         [ ${HOME}/.history -nt ${HOME}/.histfile ] && \
-         [ ${HOME}/.history -nt ${HOME}/.sh_history ]; then
-         LATESTHISTORY=`echo "${HOME}/.history"`
-    elif [ ${HOME}/.histfile -nt ${HOME}/.bash_history ] && \
-         [ ${HOME}/.histfile -nt ${HOME}/.history ] && \
-         [ ${HOME}/.histfile -nt ${HOME}/.sh_history ]; then
-         LATESTHISTORY=`echo "${HOME}/.histfile"`
-    else
-        LATESTHISTORY=`echo "${HOME}/.sh_history"`
-    fi
-    if [ -r ${LATESTHISTORY} ];then
-         FAVCOMMAND=`cat -v ${LATESTHISTORY} | sed 's/\^@//g' | \
-         sed 's/\^A//g' | sed 's/\M-//g' | sed '/^$/d' | \
-         awk '{print $1}'| sed '/#/d' | sort `
-         echo -e "\n${FAVCOMMAND}" >> $TEMP    #write file
-         COUNT="`wc -l ${LATESTHISTORY} | awk '{print $1}'`"
-         TOTAL=$((${COUNT}+$TOTAL))
-    fi
-#Rank Space
-    if [ -d  ${HOME} ]; then  #判斷Path是否存在
-        SPACES=`du -s ${HOME}`
-        ${ECHO} -e "${SPACES} $username\n" >> $TEMPSEC
-    fi
-
-done
-
-#RANK COMMAND
-RANK=`awk '{print $1}' $TEMP | sort | uniq -c | sort -n -r | head -n 20`
-rm $TEMP
 
 #Rank login times
 RANKLOGIN=`last | awk '{if(($1 != "\0")&&($1 != "wtmp")&&($1 != "boot")&& \
            ($1 != "shutdown")&&($1 != "reboot")) print $1}' | \
            sort | uniq -c | sort -n -r | awk '{printf ("%3s%10s%5s\n", NR, $2, $1)} ' | head -n 10`
 WEEKLOGIN=($(last | awk '$3=="mosh" {print $5} $4=="vi" {print $5} $4=="via" {if($5 != "mosh") print $5} \
-           $4=="via" {if($5 == "mosh") print $6} NF==10 {print $4}'| sort | uniq -c | \
-           awk '$2 == "Mon" {print "1", $2, $1} $2 == "Tue" {print "2", $2, $1} $2 == "Wed" {print "3", $2, $1} \
-           $2 == "Thu" {print "4", $2, $1} $2 == "Fri" {print "5", $2, $1} $2 == "Sat" {print "6", $2, $1} \
-           $2 == "Sun" {print "7", $2, $1}' | sort -n | awk '{print $2, $3}'))
+          $4=="via" {if($5 == "mosh") print $6} NF==10 {print $4}' | sort | uniq -c | \
+          awk '$2 == "Mon" {print "1", $2, $1} $2 == "Tue" {print "2", $2, $1} $2 == "Wed" {print "3", $2, $1} \
+          $2 == "Thu" {print "4", $2, $1} $2 == "Fri" {print "5", $2, $1} $2 == "Sat" {print "6", $2, $1} \
+          $2 == "Sun" {print "7", $2, $1}' | sort -n | awk '{print $2, $3}'))
 WEEKDAY="Mon Tue Wed Thu Fri Sat Sun"
 WEEKLOGINTIMESLIST=(0 0 0 0 0 0 0)
 for (( i = 0; i<${#WEEKLOGIN[@]};i++)); do
@@ -106,13 +62,13 @@ done
 WLOGINDAY=`echo ${WEEKDAY} | \
            awk '{printf("login time      |%4s%4s%4s%4s%4s%4s%4s" ,$1,$2,$3,$4,$5,$6,$7)}'`
 WLOGINTIMES=`echo ${WEEKLOGINSTRING} | \
-             awk '{printf("number of times |%4s%4s%4s%4s%4s%4s%4s" ,$1,$2,$3,$4,$5,$6,$7)}'`
+           awk '{printf("number of times |%4s%4s%4s%4s%4s%4s%4s" ,$1,$2,$3,$4,$5,$6,$7)}'`
 
 DAYLOGIN=($(last | awk '$3=="mosh" {print $8} $4=="vi" {print $8} \
-          $4=="via" {if($5 == "mos" || $5 == "mo") print $9} \
-          $4=="via" {if($5 != "mosh" && $5 != "mos" && $5 != "mo") print $8}  \
-          $4=="via" {if($5 == "mosh") print $9} NF==10 {print $7}' | awk -F ":" '{print $1}' | \
-          sort | uniq -c | awk '{print $2 "\t" $1}'))
+         $4=="via" {if($5 == "mos" || $5 == "mo") print $9} \
+         $4=="via" {if($5 != "mosh" && $5 != "mos" && $5 != "mo") print $8}  \
+         $4=="via" {if($5 == "mosh") print $9} NF==10 {print $7}' | awk -F ":" '{print $1}' | \
+         sort | uniq -c | awk '{print $2 "\t" $1}'))
 DAYLOGINTIMESLIST=(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
 for (( i = 0; i<${#DAYLOGIN[@]};i++)); do
   if [ ${DAYLOGIN[i]} == "00" ]; then
@@ -176,15 +132,66 @@ DLOGINTIMES=`echo ${DAYLOGINSTRING} | \
              awk '{printf("number of times |%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s%4s" \
              ,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24)}'`
 
+# Program : rank all user's commands
+
+TOTAL=0    #Count how many commands
+TEMP="$(mktemp rankCommandResult.XXXXXXX)"
+TEMPSEC="$(mktemp rankSpaceResult.XXXXXXX)"
+
+for username in $users
+do
+    USER=$username
+    HOME=`eval echo ~${USER}`
+
+    if [ ${HOME}/.bash_history -nt ${HOME}/.history ] && \
+       [ ${HOME}/.bash_history -nt ${HOME}/.histfile ] && \
+       [ ${HOME}/.bash_history -nt ${HOME}/.sh_history ]; then
+       LATESTHISTORY=`echo "${HOME}/.bash_history"`
+    elif [ ${HOME}/.history -nt ${HOME}/.bash_history ] && \
+         [ ${HOME}/.history -nt ${HOME}/.histfile ] && \
+         [ ${HOME}/.history -nt ${HOME}/.sh_history ]; then
+         LATESTHISTORY=`echo "${HOME}/.history"`
+    elif [ ${HOME}/.histfile -nt ${HOME}/.bash_history ] && \
+         [ ${HOME}/.histfile -nt ${HOME}/.history ] && \
+         [ ${HOME}/.histfile -nt ${HOME}/.sh_history ]; then
+         LATESTHISTORY=`echo "${HOME}/.histfile"`
+    else
+        LATESTHISTORY=`echo "${HOME}/.sh_history"`
+    fi
+    if [ -r ${LATESTHISTORY} ];then
+         FAVCOMMAND=`cat -v ${LATESTHISTORY} | sed 's/\^@//g' | \
+         sed 's/\^A//g' | sed 's/\M-//g' | sed '/^$/d' | \
+         awk '{print $1}'| sed '/#/d' | sort `
+         echo -e "\n${FAVCOMMAND}" >> $TEMP    #write file
+         COUNT="`wc -l ${LATESTHISTORY} | awk '{print $1}'`"
+         TOTAL=$((${COUNT}+$TOTAL))
+    fi
+#Rank Space
+    if [ -d  ${HOME} ]; then  #判斷Path是否存在
+        SPACES=`du -s ${HOME}`
+        ${ECHO} -e "${SPACES} $username\n" >> $TEMPSEC
+    fi
+
+done
+
+#RANK COMMAND
+RANK=`awk '{print $1}' $TEMP | sort | uniq -c | sort -n -r | head -n 20`
+rm $TEMP
+
 #Rank Space
 RANKSPACE=`awk '{if(($3 != NULL)&&($1 != NULL))print $1 "k\t" $3}' $TEMPSEC | \
            sort -n -r | head -n 10 | awk '{printf ("%3s%12s%7s\n", NR, $2, $1)}'`
 rm $TEMPSEC
 
-${ECHO} -e "\nTOTAL number of commands : $TOTAL\n
+if [ `whoami` == "root" ];then
+
+  ${ECHO} -e "\nTOTAL number of commands : $TOTAL\n
 Ranked commands of all users in record:\n${RANK}\n\n
-Rank user's login times : \nNo.\tUser\tTimes\n${RANKLOGIN}\n\n
-Ranked space of all users : \nNo.\tUser\tSpace\n${RANKSPACE}\n
+Ranked space of all users : \nNo.\tUser\tSpace\n${RANKSPACE}\n" >> userInformation.txt
+
+fi
+
+${ECHO} -e "\nRank user's login times : \nNo.\tUser\tTimes\n${RANKLOGIN}\n
 Daily login distribution :\n
   ${DLOGINHOUR}
   -----------------------------------------------------------------------------------------------------------------
