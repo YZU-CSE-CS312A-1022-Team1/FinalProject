010b7c9f25da10a8741e90129ac4956623871b91
Amo Chen <t9525838@gmail.com>
2015-05-25 14:56:58 +0800

    Remove extra blank

--- webVersion/userAnalysis.sh
+++ webVersion/userAnalysis.sh
@@ -147,103 +147,103 @@ if [ -d ${HOME} ]; then
   COMMANDRANK_TIMES=(`echo ${FAVCOMMAND} | awk '{print $1" "$3" "$5" "$7" "$9" "$11" "$13" "$15" "$17" "$19}'`)
 fi
 
-  if [ -f ${HTMLFILE} ];then
-    rm ${HTMLFILE}
-  fi
+if [ -f ${HTMLFILE} ];then
+  rm ${HTMLFILE}
+fi
 
-  SHELL=`finger ${ACCOUNT} | grep Shell | awk '{print $4}' | awk -F"/" '{print $NF}'`
-  LOGINTIMES=`last | grep ${ACCOUNT} | wc -l | tr -d ' '`
-  LASTLOGIN=`last |grep ${ACCOUNT} | head -n 1 | \
-             awk '{ if (match($2, /^pts/)) { if ($3 != "mosh" && $5 != "mosh") {print $4, $5, $6, $7 " from " $3} \
-             else { if ($5 == "mosh"){print $6, $7, $8, $9 " from " $3} else {print $5, $6, $7, $8" from " $3}}} \
-             else {print $3, $4, $5, $6 " from " $2}}'`
-  LOGINHOUR=`echo -e "$(last | grep ${ACCOUNT} | awk '{print $7, $1}' | awk -F ":" '$1 < 24 {print $1, $2}' | \
-             awk '{if(($1 > 0)&&($1 <= 24))print $1}')"`
-  LOGINDAY=`echo -e "$(last | grep ${ACCOUNT} |  awk '$3=="mosh" {print $5} $4=="vi" {print $5} $4=="via" \
-            {if($5 != "mosh") print $5} $4=="via" {if($5 == "mosh") print $6} NF==10 {print $4}')"`
-  DAYLOGIN=(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
-  WEEKLOGIN=(0 0 0 0 0 0 0)
-  countDailyLoginTimes
-  countWeeklyLoginTimes
-  DAYOFMONTH=`date +"%d"`
-  LOGINFREQUENCY=`echo $(bc<<<"scale=3;$LOGINTIMES/$DAYOFMONTH" | awk '{printf "%1.3f", $0}')`
-  if [ `ps au | grep ^${ACCOUNT} | wc -l |awk '{print $1}'` != 0 ]; then
-    CURRENTPROCESS=`${PS} ${ACCOUNT} | awk '{printf( \
-    "<tr><td width=60>%-10s</td><td width=60>%-10s</td><td width=60>%-10s</td><td width=60>%-10s</td><td width=60>%-10s</td><td>%-10s</td></tr>" \
-    ,$2,$3,$4,$9,$10,$11)}' | sort -g`
-  else
-    CURRENTPROCESS="<tr><td>None.</td></tr>"
-  fi
+SHELL=`finger ${ACCOUNT} | grep Shell | awk '{print $4}' | awk -F"/" '{print $NF}'`
+LOGINTIMES=`last | grep ${ACCOUNT} | wc -l | tr -d ' '`
+LASTLOGIN=`last |grep ${ACCOUNT} | head -n 1 | \
+           awk '{ if (match($2, /^pts/)) { if ($3 != "mosh" && $5 != "mosh") {print $4, $5, $6, $7 " from " $3} \
+           else { if ($5 == "mosh"){print $6, $7, $8, $9 " from " $3} else {print $5, $6, $7, $8" from " $3}}} \
+           else {print $3, $4, $5, $6 " from " $2}}'`
+LOGINHOUR=`echo -e "$(last | grep ${ACCOUNT} | awk '{print $7, $1}' | awk -F ":" '$1 < 24 {print $1, $2}' | \
+           awk '{if(($1 > 0)&&($1 <= 24))print $1}')"`
+LOGINDAY=`echo -e "$(last | grep ${ACCOUNT} |  awk '$3=="mosh" {print $5} $4=="vi" {print $5} $4=="via" \
+          {if($5 != "mosh") print $5} $4=="via" {if($5 == "mosh") print $6} NF==10 {print $4}')"`
+DAYLOGIN=(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
+WEEKLOGIN=(0 0 0 0 0 0 0)
+countDailyLoginTimes
+countWeeklyLoginTimes
+DAYOFMONTH=`date +"%d"`
+LOGINFREQUENCY=`echo $(bc<<<"scale=3;$LOGINTIMES/$DAYOFMONTH" | awk '{printf "%1.3f", $0}')`
+if [ `ps au | grep ^${ACCOUNT} | wc -l |awk '{print $1}'` != 0 ]; then
+  CURRENTPROCESS=`${PS} ${ACCOUNT} | awk '{printf( \
+  "<tr><td width=60>%-10s</td><td width=60>%-10s</td><td width=60>%-10s</td><td width=60>%-10s</td><td width=60>%-10s</td><td>%-10s</td></tr>" \
+  ,$2,$3,$4,$9,$10,$11)}' | sort -g`
+else
+  CURRENTPROCESS="<tr><td>None.</td></tr>"
+fi
 
+echo "
+      <html><head><meta http-equiv='content-Type' content='text/html; charset=UTF-8' />
+      <title>`hostname` - User behavior analysis</title>
+      <link rel='stylesheet' media='screen,print' href='../css/layout.css' />
+      <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
+      <script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/highcharts/4.1.4/highcharts.js'></script>
+      <script type='text/javascript' src='../userList.json'></script>
+      <script type='text/javascript' src='../js/userList.js'></script>
+     " >> ${HTMLFILE}
+exportCommandRank
+exportCommandRankTimes
+exportDailyLoginTimes
+exportWeeklyLoginTimes
+echo "
+      </head>
+      <body>
+      <div id='menu'>
+      <ul>
+      <div class='sidebar'>
+      <li><a href='../index.html'>System Info</a></li>
+      <li><a href='../networkTraffic.html'>Network Traffic</a></li>
+      <li><a href='../userSumUp.html'>User Summary</a></li>
+      <li style='text-decoration: none' onMouseOver='javascript:extend_2(\"user\")'>User Info</li></div>
+      <div id=user style='display: none'>
+      <ul class='hvr'>
+      <div id='userlist_user'></div>
+      </ul></div></ul></div>
+      <div id='content'>
+      <div class='title'><h1> ${ACCOUNT} </h1></div>
+      <div class='c_body'><hr>
+      <div class='leftblock'>
+      <table>
+      <tr><td width=320>Amount of directories under user's home:</td><td> $NUMOFDIR </td></tr>
+      <tr><td>Amount of files under user's home:</td><td> $NUMOFFLE </td></tr>
+      <tr><td>Default shell of this user:</td><td> $SHELL </td></tr>
+      <tr><td>Last login time: </td><td> $LASTLOGIN </td></tr>
+      <tr><td>Login times of this month:</td><td> $LOGINTIMES </td></tr>
+      <tr><td>Login frequency of this month:</td><td> $LOGINFREQUENCY time(s)/day</td></tr>
+      <tr><td>Disk space used:</td><td> $SPACE </td></tr>
+      </table>
+      <br>
+      <table><tr><td colspan=2><h2>Daily Login Distribution </h2></td></tr>
+      <tr><td>
+      <div id='login_in_day' style='width: 800px; height: 250px; margin: 0 0 0 0px'></div>
+      </td></tr>
+      </table>
+      <table><tr><td colspan=2><h2>Weekily Login Distribution</h2></td></tr>
+      <tr><td>
+      <div id='login_in_week' style='width: 700px; height: 250px; margin: 0 0 0 0px'></div>
+      </td></tr>
+      </table>
+     " >> ${HTMLFILE}
+if [ -d ${HOME} ]; then
   echo "
-        <html><head><meta http-equiv='content-Type' content='text/html; charset=UTF-8' />
-        <title>`hostname` - User behavior analysis</title>
-        <link rel='stylesheet' media='screen,print' href='../css/layout.css' />
-        <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
-        <script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/highcharts/4.1.4/highcharts.js'></script>
-        <script type='text/javascript' src='../userList.json'></script>
-        <script type='text/javascript' src='../js/userList.js'></script>
-       " >> ${HTMLFILE}
-  exportCommandRank
-  exportCommandRankTimes
-  exportDailyLoginTimes
-  exportWeeklyLoginTimes
-  echo "
-        </head>
-        <body>
-        <div id='menu'>
-        <ul>
-        <div class='sidebar'>
-        <li><a href='../index.html'>System Info</a></li>
-        <li><a href='../networkTraffic.html'>Network Traffic</a></li>
-        <li><a href='../userSumUp.html'>User Summary</a></li>
-        <li style='text-decoration: none' onMouseOver='javascript:extend_2(\"user\")'>User Info</li></div>
-        <div id=user style='display: none'>
-        <ul class='hvr'>
-        <div id='userlist_user'></div>
-        </ul></div></ul></div>
-        <div id='content'>
-        <div class='title'><h1> ${ACCOUNT} </h1></div>
-        <div class='c_body'><hr>
-        <div class='leftblock'>
-        <table>
-        <tr><td width=320>Amount of directories under user's home:</td><td> $NUMOFDIR </td></tr>
-        <tr><td>Amount of files under user's home:</td><td> $NUMOFFLE </td></tr>
-        <tr><td>Default shell of this user:</td><td> $SHELL </td></tr>
-        <tr><td>Last login time: </td><td> $LASTLOGIN </td></tr>
-        <tr><td>Login times of this month:</td><td> $LOGINTIMES </td></tr>
-        <tr><td>Login frequency of this month:</td><td> $LOGINFREQUENCY time(s)/day</td></tr>
-        <tr><td>Disk space used:</td><td> $SPACE </td></tr>
-        </table>
-        <br>
-        <table><tr><td colspan=2><h2>Daily Login Distribution </h2></td></tr>
-        <tr><td>
-        <div id='login_in_day' style='width: 800px; height: 250px; margin: 0 0 0 0px'></div>
+        <table><tr><td colspan='2'><h3>Most commonly used command</h3></td></tr>
+        <tr><td colspan='2'>(analyzed by last $TOTALCOMMAND commands in record):</td></tr>
+        <tr><td><br>
+        <div id='user_command_chart' style='width: 800px; height: 350px; margin: 0 0 0 0px' ></div>
         </td></tr>
         </table>
-        <table><tr><td colspan=2><h2>Weekily Login Distribution</h2></td></tr>
-        <tr><td>
-        <div id='login_in_week' style='width: 700px; height: 250px; margin: 0 0 0 0px'></div>
-        </td></tr>
-        </table>
        " >> ${HTMLFILE}
-  if [ -d ${HOME} ]; then
-    echo "
-          <table><tr><td colspan='2'><h3>Most commonly used command</h3></td></tr>
-          <tr><td colspan='2'>(analyzed by last $TOTALCOMMAND commands in record):</td></tr>
-          <tr><td><br>
-          <div id='user_command_chart' style='width: 800px; height: 350px; margin: 0 0 0 0px' ></div>
-          </td></tr>
-          </table>
-         " >> ${HTMLFILE}
-  fi
-  echo "
-        <br>
-        <table><tr><td colspan='6'><h3>Currently running process:</h3></td></tr>
-        $CURRENTPROCESS
-        </table>
-        </div>
-        </div>
-        </div>
-        </body></html>
-       " >> ${HTMLFILE}
+fi
+echo "
+      <br>
+      <table><tr><td colspan='6'><h3>Currently running process:</h3></td></tr>
+      $CURRENTPROCESS
+      </table>
+      </div>
+      </div>
+      </div>
+      </body></html>
+     " >> ${HTMLFILE}
